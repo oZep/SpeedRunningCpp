5e4889e98f1d9fbfd5c676043e409b60987e498d
joeyissa <joeyissa59@gmail.com>
2023-09-04 13:30:19 -0400

    working with datatypes

---
+++ dataTypes.cpp
@@ -0,0 +1,32 @@
+# include <iostream>
+using namespace std;
+
+// cd /Users/joeyissa/Downloads/LearningCplus
+// ./dataTypes
+
+// working with data types 
+
+
+
+int main() {
+    int myNum = 5; // 2 or 4 bytes
+    float myFloatNum = 5.99; // Floating point number 4 bytes, Sufficient for storing 6-7 decimal digits
+    double myDoubleNum = 9.98;  // Floating point number 8 bytes, Sufficient for storing 15 decimal digits
+    char myLetter = 'D';
+    bool myBoolean = true;
+    string myText = "Hello";
+
+    char a = 65, b = 66, c = 67;
+    cout << a  << endl;
+    cout << b  << endl;
+    cout << c  << endl;
+
+}
+
+// the difference between float and double is it's precision e.g how many digits after the decimal place it can have
+// double is better for most calculations then float
+// 1 --> true, 0 --> false for boolean outputs
+// ou can use ASCII values to display certain characters
+// C++ uses the same operators as Java/python
+
+
