d3f68dc9c72e413176de1726bbaac8d5b29a2635
joeyissa <joeyissa59@gmail.com>
2023-09-04 16:11:10 -0400

    string operations

---
+++ strings.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string x = "hello world";
+    int y = x.length();  // get length of string
+
+    cout << x[0] << endl; // can access parts of strings (like in python)
+
+    x[0] = 'J'; // can change characters of the string
+    cout << x;
+
+
+    return 0;
+
+}
+
+// use the same methods to write special characters within strings
+
+/* 
+using namespace std line can be omitted and replaced with the std keyword, 
+followed by the :: operator for string (and cout) objects
+*/
